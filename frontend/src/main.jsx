69617d834cb9d59b7fa8ee97a7825d8039d16838
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2024-10-16 10:24:54 -0700

    rebase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Import React Router
 import App from './App.jsx';
-import Login from './Pages/Login.jsx'; 
+import Login from "./pages/Login.jsx";
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        {/* Route for the home page */}
         <Route path="/" element={<App />} />
-        
-        {/* Route for the login page */}
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
