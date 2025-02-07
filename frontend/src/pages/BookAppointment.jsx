c8e22c4b6b0183382006c503d2ee5f77b4f3f1b8
Kiezroy <kyletnguyen16@gmail.com>
2025-02-07 14:03:35 -0800

    TF-82
    Fixed issues when configuring with MongoDB Compass

--- frontend/src/pages/BookAppointment.jsx
+++ frontend/src/pages/BookAppointment.jsx
@@ -1,43 +1,43 @@
-import React, {useState, useEffect } from 'react';
-import {Typography, Grid2, Button, Box} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Typography, Grid2, Button, Box } from '@mui/material';
 import AppointmentCard from '../components/AppointmentCard.jsx';
-import {formatDuration} from '../utilities/formatDuration.js';
+import { formatDuration } from '../utilities/formatDuration.js';
 import services from '../temporarydata/Services.jsx';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const categories = ['Body Treatments', 'Facials', 'Nanochanneling Facials', 'Hydrafacials', 'Dermaplaning', 'Dermabrasion', 'Nail Care', 'Massages', 'Waxing', 'Add-Ons'];
 
 const BookAppointment = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [selectedCategory, setSelectedCategory] = useState('Body Treatments');
-    const [isDataPopulated, setIsDataPopulated] = useState(false); // Track if data is populated
+  const [selectedCategory, setSelectedCategory] = useState('Body Treatments');
+  const [isDataPopulated, setIsDataPopulated] = useState(false); // Track if data is populated
 
-    // Function to trigger populating data in MongoDB
-    const handlePopulateData = async () => {
-        try {
-            // Check if the data is already populated (only need to run once)
-            const response = await axios.get('http://localhost:5000/api/appointments/check-existence');
-            if (response.data.exists) {
-                console.log('Data already populated.');
-                setIsDataPopulated(true);
-                return;
-            }
+  // Function to trigger populating data in MongoDB
+  const handlePopulateData = async () => {
+    try {
+      // Check if the data is already populated (only need to run once)
+      const response = await axios.get('http://localhost:5000/api/appointments/check-existence');
+      if (response.data.exists) {
+        console.log('Data already populated.');
+        setIsDataPopulated(true); // Mark as populated
+        return;
+      }
 
-            const formattedAppointments = Object.values(services[0]).map(service => {
-                const appointmentData = {
-                    name: service.name || "Unknown",
-                    pricing: service.pricing !== undefined ? service.pricing : 0,
-                    duration: service.duration || "0:30:00",
-                    category: service.category || "Unknown",
-                    description: service.description && service.description.length > 0 ? service.description.join(' ') : "No description available"
-                };
-                return appointmentData;
-            });
+      const formattedAppointments = Object.values(services[0]).map(service => {
+        const appointmentData = {
+          name: service.name || "Unknown",
+          pricing: service.pricing !== undefined ? service.pricing : 0,
+          duration: service.duration || "0:30:00",
+          category: service.category || "Unknown",
+          description: service.description && service.description.length > 0 ? service.description.join(' ') : "No description available"
+        };
+        return appointmentData;
+      });
 
-            // Post the new data
-            const postResponse = await axios.post('http://localhost:5000/api/appointments/populate', { appointments: formattedAppointments });
+      // Post the new data
+      const postResponse = await axios.post('http://localhost:5000/api/appointments/populate', { appointments: formattedAppointments });
       if (postResponse.status === 201) {
         console.log('Appointments populated successfully');
         setIsDataPopulated(true);
@@ -49,103 +49,77 @@ const BookAppointment = () => {
     }
   };
 
-    useEffect(() => {
-        if (!isDataPopulated) {
-        handlePopulateData();
-        }
-    }, [isDataPopulated]); // This will run once and only if data is not populated
+  // Effect to check if data has been populated and populate if not
+  useEffect(() => {
+    if (!isDataPopulated) {
+      handlePopulateData();
+    }
+  }, [isDataPopulated]); // This will run once and only if data is not populated
 
+  const handleAppointmentBookConfirm = (appointmentData) => {
+    navigate('/payment', { state: { appointmentData } });
+  };
 
-    // Navigate to payment page and pass the data via state
-    const handleAppointmentBookConfirm = (appointmentData) => {
-        navigate('/payment', { state: { appointmentData } });
-    };
+  return (
+    <Box style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Box>
+        <Typography variant="h3" gutterBottom align="center" sx={{ fontWeight: 'bold', fontFamily: "Special Elite" }} xs={12} sm={6} md={4} p={5}>
+          Book Appointment
+        </Typography>
 
+        <Grid2 container spacing={2} justifyContent="center" alignItems="center">
+          {categories.map((category, index) => (
+            <Grid2 item xs={12} sm={6} md={4} key={index}>
+              <Button
+                variant="outlined"
+                style={{
+                  padding: '10px',
+                  textAlign: 'center',
+                  backgroundColor: selectedCategory === category ? '#1976d2' : '',
+                  color: selectedCategory === category ? 'white' : '',
+                }}
+                onClick={() => setSelectedCategory(category)}
+              >
+                {category}
+              </Button>
+            </Grid2>
+          ))}
+        </Grid2>
 
-    return (
+        <Box p={5}>
+          <Grid2 container direction="row" sx={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+            {Object.values(services[0]).map((service, index) => {
+              const description = service.description;
+              const category = service.category;
+              const duration = service.duration;
 
-        // Centers the whole page
-        <Box style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-
-            {/* Turns the title and category into a column */}
-            <Box>
-
-                {/* Title */}
-                <Typography variant="h3" gutterBottom align="center" sx={{ fontWeight: 'bold',fontFamily: "Special Elite"}}  xs={12} sm={6} md={4} p={5}>
-                    Book Appointment
-                </Typography>
-
-                {/* Centers the categories and put spacing between them */}
-                <Grid2 container spacing={2} justifyContent="center" alignItems="center">
-                    {categories.map((category, index) => (
-
-                        // Add padding to the buttons
-                        <Grid2 item xs={12} sm={6} md={4} key={index}>
-
-                            {/* Button */}
-                            <Button
-                                variant="outlined"
-
-                                // Background color changes to blue when the category is selected
-                                // Text color changes to white when the category is selected
-                                style={{
-                                    padding: '10px',
-                                    textAlign: 'center',
-                                    backgroundColor: selectedCategory === category ? '#1976d2' : '', // Active button color
-                                    color: selectedCategory === category ? 'white' : '', // Text color for active button
-                                }}
-
-                                // Updates category to the current one
-                                onClick={() => setSelectedCategory(category)}
-                            >
-                                {category}
-                            </Button>
-                        </Grid2>
-                    ))}
-                </Grid2>
-
-                {/* Bottom padding */}
-                <Box p={5}>
-
-                    {/* Makes the cards stack horizontally and keep them centered */}
-                    <Grid2 container direction="row" sx={{
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}>
-
-                        {Object.values(services[0]).map((service, index) => {
-                            const description = service.description; // Get the array descriptions of the service
-                            const category = service.category; // Get the category of the service
-                            const duration = service.duration; // Get the duration of the service
-
-                            {/* Check if the category is selected */}
-                            if (category === selectedCategory) {
-                                return (
-                                    <Grid2 item sx={{p: 1.5}} key={index}>
-                                        <AppointmentCard
-                                            title={service.name}
-                                            description={description.map((line, lineIndex) => (
-                                                <span key={lineIndex}>
-                                                    {line}
-                                                    <br/> {/* Add line break after each line */}
-                                                </span>
-                                            ))}
-                                            pricing={service.pricing}
-                                            duration={formatDuration(duration)} // Set the duration of the service and format it
-                                            onAppointmentBookConfirm={() => handleAppointmentBookConfirm(service)} // Pass data to the payment page
-                                        />
-                                    </Grid2>
-                                );
-                            }
-
-                        })}
-                    </Grid2>
-
-                </Box>
-
-            </Box>
+              if (category === selectedCategory) {
+                return (
+                  <Grid2 item sx={{ p: 1.5 }} key={index}>
+                    <AppointmentCard
+                      title={service.name}
+                      description={description.map((line, lineIndex) => (
+                        <span key={lineIndex}>
+                          {line}
+                          <br />
+                        </span>
+                      ))}
+                      pricing={service.pricing}
+                      duration={formatDuration(duration)}
+                      onAppointmentBookConfirm={() => handleAppointmentBookConfirm(service)}
+                    />
+                  </Grid2>
+                );
+              }
+            })}
+          </Grid2>
         </Box>
-    );
+      </Box>
+    </Box>
+  );
 }
 
 export default BookAppointment;
