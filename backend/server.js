ec02b4d478a2c492e455613064032129fb172e4e
anhqtr <annahqtran@gmail.com>
2025-02-09 13:18:22 -0800

    Created separate file for instagram routes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const { protect, adminOnly } = require('./middleware/authMiddleware');
 
 dotenv.config();
 connectDB();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -15,6 +14,9 @@ app.use(express.json());
 // Routes
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
+
+const instagramRoutes = require('./routes/instagramRoutes');
+app.use('/api/instagram', instagramRoutes);
 
 // Admin route
 app.get('/api/admin-dashboard', protect, adminOnly, (req, res) => {
@@ -24,61 +26,6 @@ app.get('/api/admin-dashboard', protect, adminOnly, (req, res) => {
 // Test Route
 app.get('/', (req, res) => res.send('API is running'));
 
-// Instagram route
-app.get('/auth',(req, res) => {
-  const authURI = 'https://api.instagram.com/oauth/authorize?client_id=${process.env.REACT_APP_ACCESS_CODE}&redirect_uri=${process.env.REACT_APP_INSTAGRAM_REDIRECT}&scope=user_profile,user_media&response_type=code';
-  res.redirect(authURI);
-})
-app.get('/auth/callback', async (req, res) => {
-  const {code} = req.query;
-  try {
-    const response = await axios.post('https://api.instagram.com/oauth/access_token', {
-      client_id: process.env.REACT_APP_ACCESS_CODE,
-      client_secret: process.env.REACT_APP_IG_SECRET,
-      grant_type: 'authorization_code',
-      redirect_uri: process.env.REACT_APP_INSTAGRAM_REDIRECT,
-      code,
-    });
-    const {access_token} = response.data;
-    res.send('Access Token: ${access_token}');
-  } catch (error) {
-    res.status(500).send('Error');
-  }
-});
-
-// Fetch Instagram Profile
-app.get('/user', async (req, res) => {
-  const {access_token} = req.query;
-  try {
-    const response = await axios.get('https://graph.instagram.com/me?fields=id,username&access_token=${access_token}');
-     
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error');
-  }
-});
-
-// Fetch Instagram Media Data
-app.get('/media', async (req, res) => {
-  const {access_token} = req.query;
-  try {
-    const response = await axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type&access_token=${access_token}');
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error');
-  }
-});
-
-// IG Token Refresh
-app.get('/refresh_token', async (req, res) => {
-  const {refresh_token} = req.query;
-  try {
-    const response = await axios.get('https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${refresh_token}');
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error');
-  }
-});
 
 // Start Server
 const PORT = process.env.PORT || 5000;
