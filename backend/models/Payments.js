6c8f365c4a054bcb20477554b92cf91d34fb2352
Sammy <106267646+ucsammy@users.noreply.github.com>
2025-03-11 20:26:19 -0700

    Implemented /list for retrieving all payments

--- backend/models/Payments.js
+++ backend/models/Payments.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const PaymentSchema = new mongoose.Schema({
+const PaymentSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
     paymentId: { type: String, required: true, unique: true },
     status: { type: String, required: true },
     amount: { type: Number, required: true },
     currency: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Payment", PaymentSchema);
