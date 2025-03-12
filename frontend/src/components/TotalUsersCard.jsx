acd28e8aa41628b6aaf6fe88bd41356df9e6408d
anhqtr <annahtran@csus.edu>
2025-03-11 20:01:18 -0700

    component to display total users

---
+++ frontend/src/components/TotalUsersCard.jsx
@@ -0,0 +1,38 @@
+
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+
+const TotalUsersCard = () => {
+    const [totalUsers, setTotalUsers] = useState(0);
+
+    useEffect(() => {
+        const fetchTotalUsers = async () => {
+            try {
+                const response = await axios.get("/api/statistics");
+                console.log("Total Users API response:", response.data);
+                setTotalUsers(response.data.totalUsers);
+            } catch (error) {
+                console.error("Error fetching total users:", error);
+            }
+        };
+        fetchTotalUsers();
+    }, []);
+
+    return (
+        <Card>
+            <CardContent>
+                <Typography variant="h6">
+                    Total Users: 
+                </Typography>
+                  <Typography variant='body2' sx={{ color: 'text.secondary', }}>
+                    {totalUsers} users have registered on the platform.
+                    </Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default TotalUsersCard;
