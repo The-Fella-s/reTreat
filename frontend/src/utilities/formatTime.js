9fab80bc0fcd1ec26a7409e51fea29d19a38d378
Unalphabetical <35241506+Unalphabetical@users.noreply.github.com>
2025-04-23 20:48:49 -0700

    Remove more unused frontend stuff

--- frontend/src/utilities/formatTime.js
+++
@@ -1,8 +0,0 @@
-// This function takes a duration string in the format "HH:MM" and returns an AM/PM string
-// such as "1:00" to "1 am" or "13:00" to "1 pm"
-export const formatTimeToAmPm = (time) => {
-    const [hour, minute] = time.split(':').map(Number);
-    const amPm = hour >= 12 ? 'PM' : 'AM';
-    const formattedHour = hour % 12 || 12; // Convert 0 to 12 for midnight
-    return `${formattedHour}:${minute.toString().padStart(2, '0')} ${amPm}`;
-};
